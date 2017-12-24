31f0b0941fe92392560a991f70493af2676cb5d9
Adrian Pankiewicz <farami11@tlen.pl>
2017-12-24 15:13:12 +0100

    Primefaces setup

--- src/main/java/pl/exam/app/Configuration.java
+++ src/main/java/pl/exam/app/Configuration.java
@@ -3,7 +3,7 @@ package pl.exam.app;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("")
+@ApplicationPath("api")
 public class Configuration extends Application
 {
 }
